872d60b127885c0f470c65357b7f31648f0dbf7c
moneya <moneya59@gmail.com>
2019-09-17 07:24:14 +0100

    finised the BE

---
+++ errors.php
@@ -0,0 +1,12 @@
+<?php
+// require('action/login.php');
+// require('action/reg.php');
+require('action/auth.php');
+?>
+<?php  if (count($errors) > 0) : ?>
+  <div class="error">
+  	<?php foreach ($errors as $error) : ?>
+  	  <p><?php echo $error ?></p>
+  	<?php endforeach ?>
+  </div>
+<?php  endif ?>
