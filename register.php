8fc71e160d84c76b6ed073f92625640d8782cdf6
faruq2 <faruqyusuff437@gmail.com>
2019-09-18 15:00:35 +0100

    Made changes to register.php and master css

--- register.php
+++ register.php
@@ -9,7 +9,7 @@ require('action/auth.php');
 <?php  if (isset($_SESSION['username'])) {
      header('Location: welcome');
 }
-     
+
     ?>
 
 <html lang="en">
@@ -24,31 +24,30 @@ require('action/auth.php');
 
 <body>
     <!-- images  -->
+    <!-- images  -->
+    <img class="top-left" src="https://res.cloudinary.com/fabianuzukwu/image/upload/v1568717437/teamalpha/feo7opljnv8fzthljrb5.png" alt="">
+    <img class="top-right" src="https://res.cloudinary.com/fabianuzukwu/image/upload/v1568717437/teamalpha/lrnzpp5pqrvgcvdq436d.png" alt="">
+    <img class="bottom-left" src="https://res.cloudinary.com/fabianuzukwu/image/upload/v1568717437/teamalpha/upm7yc8p4q1vo2gjpo6y.png" alt="">
+    <img class="bottom-right" src="https://res.cloudinary.com/fabianuzukwu/image/upload/v1568717437/teamalpha/y2jrfxzyu8t25heg5lfr.png" alt="">
+
+
+
     <div class="container">
-        <img class="top-left" src="images/Subtract.png" alt="">
-        <img class="top-right" src="images/Subtract (1).png" alt="">
-        <img class="bottom-left" src="images/Subtract (3).png" alt="">
-        <img class="bottom-right" src="images/Subtract (2).png" alt="">
-    </div>
+      <div class="forms-container">
 
-
-    <div class="parent">
-
-        <form class="quote" method="post" action="">
-            <header>
-                <h2>Sign Up</h2>
-
-            </header>
+          <form class="log-in" method="post" action="">
+            <div>
+                <h2>Log In</h2>
+            </div>
             <div class="error">
             <?php if(!empty($_SESSION['msg'] ) ){
             echo  "You must log in first";
             unset($_SESSION['msg']);
             }
-            
+
                 ?>
             <?php include('errors.php'); ?>
             </div>
-
             <div>
                 <input type="email" name="email" value="<?php echo $email; ?>" placeholder="Email Address">
             </div>
@@ -65,7 +64,7 @@ require('action/auth.php');
 
                 <input type="text" name="lastname" value="<?php echo $lastname; ?>" placeholder="Last Name">
             </div>
-            
+
             <div>
 
                 <input type="password" name="password"  placeholder="Password">
@@ -78,14 +77,7 @@ require('action/auth.php');
             <button class="button_1" name="reg" type="submit">Become an Alpha</button>
             <p class="have-act">I Already Have An Account?.<a href="/" class="active">Log In</a></p>
         </form>
+      </div>
     </div>
-
-
-
-
-
-
-
 </body>
-
 </html>
